82df4a411830443ae8d57a385ae60428c0570835
maxgigi <max.grdt@gmail.com>
2025-06-18 15:59:52 +0200

    Student.dart: les ENP existent maintenant

--- lib/model/Student.dart
+++ lib/model/Student.dart
@@ -67,6 +67,9 @@ class Student {
     else if (specialization.contains("STI")) {
       departement = "STI";
     }
+    else if (specialization.contains("ENP")) {
+      this.departement = "ENP";
+    }
     else {
       departement = "GSI";
     }
